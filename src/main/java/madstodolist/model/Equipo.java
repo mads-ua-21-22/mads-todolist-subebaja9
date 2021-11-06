66944d7c4f28f83497ace75d2037fab697c9611c
subebaja9 <ioc8@gcloud.ua.es>
2021-11-06 18:01:16 +0100

    Segundo test Equipo + id

--- src/main/java/madstodolist/model/Equipo.java
+++ src/main/java/madstodolist/model/Equipo.java
@@ -1,9 +1,23 @@
 package madstodolist.model;
 
-public class Equipo {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "equipos")
+public class Equipo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
     private String nombre;
 
     public Equipo(String nombre) {this.nombre=nombre;}
 
     public String getNombre(){return nombre;}
+
+    public Long getId(){return id;}
 }
--- src/test/java/madstodolist/EquipoTest.java
+++ src/test/java/madstodolist/EquipoTest.java
@@ -1,16 +1,35 @@
 package madstodolist;
 
 import madstodolist.model.Equipo;
+import madstodolist.model.EquipoRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class EquipoTest {
+    @Autowired
+    private EquipoRepository equipoRepository;
+
     @Test
     public void crearEquipo(){
         Equipo equipo = new Equipo("Proyecto P1");
         assertThat(equipo.getNombre()).isEqualTo("Proyecto P1");
+    }
+
+    @Test
+    @Transactional
+    public void grabarEquipo(){
+        //GIVEN
+        Equipo equipo = new Equipo("Proyecto P1");
+
+        //WHEN
+        equipoRepository.save(equipo);
+
+        //THEN
+        assertThat(equipo.getId()).isNotNull();
     }
 }
