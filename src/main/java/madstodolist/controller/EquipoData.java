0578aa9607e99c84ea3c9aba4024c208574cae4a
subebaja9 <ioc8@gcloud.ua.es>
2021-11-07 23:33:32 +0100

    Añadido EquipoData

---
+++ src/main/java/madstodolist/controller/EquipoData.java
@@ -0,0 +1,9 @@
+package madstodolist.controller;
+
+public class EquipoData {
+    private String titulo;
+
+    public String getTitulo() {return titulo;}
+
+    public void setTitulo(String titulo){this.titulo=titulo;}
+}
